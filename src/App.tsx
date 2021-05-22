21b7e71431c72f77e01cd7aa71a4da1f6b53a996
Luiz Santiago <luiz.santiago@championx.com>
2021-05-21 21:16:18 -0300

    Added films

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,105 @@ interface Movie {
 
 const movies: Movie[] = [
   {
-    name: "Star Wars III",
+    name: "Monty Python - E o busca pelo calice sagrado",
     imageLocation: "./img/mini1.jpg",
-    recommendedBy: "Luiz Augusto",
+    recommendedBy: "Luiz Augusto"
   },
+    {
+    name: "Monty Python - Vida de Brian",
+    imageLocation: "./img/mini2.jpg",
+    recommendedBy: "Luiz Augusto"
+  },
+    {
+    name: "Detroid Rock City",
+    imageLocation: "./img/mini3.jpg",
+    recommendedBy: "Luiz Augusto"
+  },
+    {
+    name: "Cães de Aluguel",
+    imageLocation: "./img/mini4.jpg",
+    recommendedBy: "Luiz Augusto"
+  },
+    {
+    name: "Sete homens e um destino",
+    imageLocation: "./img/mini5.jpg",
+    recommendedBy: "Luiz Augusto"
+  },
+    {
+    name: "O contador",
+    imageLocation: "./img/mini6.jpg",
+    recommendedBy: "Bianca"
+  },
+   {
+    name: "Shrek",
+    imageLocation: "./img/mini7.jpg",
+    recommendedBy: "Bianca"
+  },
+   {
+    name: "Spirit O corcel Indomável",
+    imageLocation: "./img/mini8.jpg",
+    recommendedBy: "Bianca"
+  },
+   {
+    name: "Fuja",
+    imageLocation: "./img/mini9.jpg",
+    recommendedBy: "Bianca"
+  },
+   {
+    name: "O lobo de wallstreet",
+    imageLocation: "./img/mini10.jpg",
+    recommendedBy: "Bianca"
+  },
+  {
+    name: "Tom and jerry",
+    imageLocation: "./img/mini11.jpg",
+    recommendedBy: "Suellen"
+  },
+  {
+    name: "Sociedade da justiça segunda guerra mundial",
+    imageLocation: "./img/mini12.jpg",
+    recommendedBy: "Suellen"
+  },
+  {
+    name: "Zack Snyder's Justice League",
+    imageLocation: "./img/mini13.jpg",
+    recommendedBy: "Suellen"
+  },
+  {
+    name: "Scooby doo e a espada",
+    imageLocation: "./img/mini14.jpg",
+    recommendedBy: "Suellen"
+  },
+  {
+    name: "Willy's Wonderland",
+    imageLocation: "./img/mini15.jpg",
+    recommendedBy: "Suellen"
+  },
+  {
+	name: "Day of the dead (85)",
+    imageLocation: "./img/mini16.jpg",
+    recommendedBy: "Conrado"
+  },
+  {
+    name: "Schools of Rock",
+    imageLocation: "./img/mini17.jpg",
+    recommendedBy: "Conrado"
+  },
+  {
+	name: "Dead Society Poets",
+    imageLocation: "./img/mini18.jpg",
+    recommendedBy: "Conrado"
+  },
+  {
+	name: "Scary Movie",
+    imageLocation: "./img/mini19.jpg",
+    recommendedBy: "Conrado"
+  },
+  {
+   name: "Gremlins",
+    imageLocation: "./img/mini20.jpg",
+    recommendedBy: "Conrado"
+  }
 ];
 
 function App() {
@@ -37,23 +132,18 @@ function App() {
   function startRotation() {
     setIsStart(false);
     setIsRotate(true);
-    rodandoRodando(true)
   };
 
   function stopRotation() {
     setIsStart(true);
     setIsRotate(false);
-    rodandoRodando(false)
+    rodandoRodando()
   };
 
-  function rodandoRodando(stop:boolean) {
+  function rodandoRodando() {
     let i = 0;
-    while(stop){
-      i= Math.floor(Math.random() * (20 - 1)) + 1;
-    }
-    if(!stop){
+      i= Math.floor(Math.random() * (19 - 0)) + 0;
       setIndex(i);
-    }
   };
 
   function createManyPieces(n = 20) {
@@ -107,7 +197,7 @@ function App() {
   return (
     <div >
       <h3 className="title">Noite de filme!</h3>
-      <h3 className={isStart ? "title" : "hidden"}>Vencedor: {index} </h3>
+      <h3 className={isStart ? "title" : "hidden"}>Vencedor: {movies[index].name} </h3>
       <h3 className={isRotate ? "title" : "hidden"}>Rodando rodando</h3>
       <Circle className={isRotate ? "start-rotate" : "start-rotate stop-rotate"}>
         <ul>{createManyPieces()}</ul>
